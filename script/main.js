3615dd5ec47816b2f97056de319fdb48f2ad8e0d
Muhammad Berdiansyah <berdiansyah34@gmail.com>
2024-10-12 20:14:36 +0700

    first commit

--- script/main.js
+++ script/main.js
@@ -256,6 +256,7 @@ const animationTimeline = () => {
         zIndex: "-1",
     })
     .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .from(".message button", 1, ideaTextTrans, 1.2)
     .to(
         ".last-smile",
         0.5, {
